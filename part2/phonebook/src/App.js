bfa30ad5e5cacb6f08127024b7da5bc6e721b7e7
Raphaël Kanapitsas <raphaelk@tuta.io>
2022-06-19 16:54:22 +0200

    phonebook step1 done

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = { name: newName }
+    if (persons.findIndex(e => e.name === newName) === -1) {
+      setPersons(persons.concat(newPerson))
+    }
+    else {alert(`${newName} is already in the phonebook`)}
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(p => <li key={p.name}>{p.name}</li>)}
+      </ul>
+      <div> debug: {newName} </div>
+    </div>
+  )
+}
+
+export default App
